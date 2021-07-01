dbc35c60e08a0589c03e0381d56c99714225a613
nickcrosby <ncrosby2000@gmail.com>
2021-07-01 17:30:07 +1000

    Exercise 1.1: Course information, step 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//Here, the function defining the component has a parameter (props)
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
-  )
-}
-
-//React component with the name 'App'
 const App = () => {
+  const course = 'Half Stack Application development'
+  const part1 = 'Fundemantals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
