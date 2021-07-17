999597d53ab2caf663662e2718d4dd2512f22ed8
nickcrosby <ncrosby2000@gmail.com>
2021-07-17 17:54:20 +1000

    Passing state to child components

--- src/index.js
+++ src/index.js
@@ -2,30 +2,51 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+// this 'Display' component is responsible for displaying the value of the counter
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+// button component for all the buttons of our application
+// here we need to pass the event handler (onClick) as well as the title of the button through the component's props
+const Button = (props) => {
+  // note: you have to include a return statement for any component that renders anything
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 // here we are adding a state to our application's App component with the help of React's state hook
 const App = () => {
+  const [ counter,setCounter ] = useState(0)
 
-  const [ counter, setCounter] = useState(0)
-
-  // increments the counter everytime the button named 'plus' is pressed
   const increaseByOne = () => setCounter(counter + 1)
-
-  // sets the counter to '0' everytime the button named 'zero' is pressed
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   console.log('rendering...', counter)
 
-  // everytime the setCounter modifies the state it causes the component to rerender
-  // the value of the onClick attributes for each button is a variable containing a reference to 2 existing functions
+  // the application's state is stored in App, but it is passed down to the Display component through props
+  // using the component is straightforward, as we only need to pass the state of the counter to it
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
