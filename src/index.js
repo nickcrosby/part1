b84af34d509814df1334afaa2b9302fd344ed882
nickcrosby <ncrosby2000@gmail.com>
2021-07-17 17:28:04 +1000

    Event handler is a function

--- src/index.js
+++ src/index.js
@@ -4,35 +4,26 @@ import ReactDOM from 'react-dom'
 
 // here we are adding a state to our application's App component with the help of React's state hook
 const App = () => {
-  // the component refinition begins with the function call useState(0)
-  // the function call adds state to the component and initialises it with the value of 0
-  // the function returns an array with two items
-  // we assign the items to the variables counter and setCounter using destructuring
+
   const [ counter, setCounter] = useState(0)
-  // counter variable is assigned the initial value of state which is 0
-  // setCounter variable is assigned to a function that will be used to modify the state
 
-  // this is an event handler, which will perform a function everytime a button is clicked
-  // see the button under return (), everytime this button is pressed, this function is called
-  
-  /*
-  const handleClick = () => {
-    setCounter(counter + 1)
-  }
-  */
+  // increments the counter everytime the button named 'plus' is pressed
+  const increaseByOne = () => setCounter(counter + 1)
 
-  // we can log the values of the component's variables to the console
-  // this allows us to debug and test if something is going wrong
+  // sets the counter to '0' everytime the button named 'zero' is pressed
+  const setToZero = () => setCounter(0)
+
   console.log('rendering...', counter)
 
   // everytime the setCounter modifies the state it causes the component to rerender
+  // the value of the onClick attributes for each button is a variable containing a reference to 2 existing functions
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         plus
       </button>
-      <button onClick={() => setCounter(0)}>
+      <button onClick={setToZero}>
         zero
       </button>
     </div>
