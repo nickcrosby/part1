5f1ecb974ca9a14d215e70b49a3be25edc93f4ef
nickcrosby <ncrosby2000@gmail.com>
2022-04-19 21:17:45 +1000

    Current state 19.04.22

--- src/index.js
+++ src/index.js
@@ -1,44 +1,31 @@
-// here, we import the useState function
+// here, we import the useState function.
+// useState allows us to change the state of components
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name, age }) => {
-  // since we reference props.x a few times in the code, it can help to streamlinse if we assign the
-  // values of the properties into two variables name and age which we can then use in our code.
-  // we can use destructing (as shown above) to make the assignment of variables even easier.
-
-  // the logic for guessing the year of birth is seperated into its own function.
-  // the person's age does not have to be passed as a parameter, since the function
-  // can directly access all props that are passed to the component (Hello).
-  const bornYear = () => new Date().getFullYear() - age // the date function built-in function for getting the current year (2022)
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  // the function body that defines the component begins with the function call.
+  // the counter variables are assigned the initial value of state which is 0.
+  const [ counter, setCounter ] = useState(0)
 
+  // the application calls the setTimeout function and passes it two parameters:
+  // a function to increment the counter state and a timeout of one second.
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000   // the 1000 here denotes 1 second.
+  )
+
+  console.log('rendering...', counter)
+
+  // when the state modifying setCounter is called, React re-renders the component
+  // which means that the function body of the component gets re-executed.
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
 // this command renders the contents of the <App /> component into the div-element,
-// as defined in the file public/index.html, having the id value 'root'
+// as defined in the file public/index.html, having the id value 'root'.
 ReactDOM.render(
   <App />, document.getElementById('root')
 )
